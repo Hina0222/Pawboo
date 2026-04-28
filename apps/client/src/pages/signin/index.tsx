a4718d98130177e239c9dda9e4f47f77bf883ac4
Hina0222 <hina0222@naver.com>
2026-04-28 21:25:01 +0900

    feat[client]: 비로그인 기능 삭제

--- apps/client/src/pages/signin/index.tsx
+++ apps/client/src/pages/signin/index.tsx
@@ -4,7 +4,6 @@ import { useTranslations } from 'next-intl';
 import LoginLogoImg from '@/shared/assets/images/LoginLogoImg.png';
 import LoginMainImg from '@/shared/assets/images/LoginMainImg.png';
 import KakaoIcon from '@/shared/assets/icons/KakaoIcon.svg';
-import { Link } from '@/app/i18n/navigation';
 
 export default function SigninPage() {
   const t = useTranslations('signin');
@@ -27,7 +26,7 @@ export default function SigninPage() {
       </div>
 
       {/* 하단 로그인 버튼 */}
-      <div className="flex flex-col gap-4 px-4">
+      <div className="flex flex-col gap-4 px-4 py-6">
         <button
           onClick={handleKakaoLogin}
           className="flex w-full items-center justify-center gap-3 rounded-[16px] bg-[#FEE500] py-3 text-base font-semibold text-[#000000]"
@@ -40,12 +39,6 @@ export default function SigninPage() {
           <span className="underline underline-offset-2">{t('privacy')}</span>
           {t('termsSuffix')}
         </p>
-      </div>
-
-      <div className="mx-auto mt-7.5 mb-6">
-        <Link href={'/'} className="text-[#E1E1E3] underline">
-          먼저 둘러볼래요
-        </Link>
       </div>
     </>
   );
